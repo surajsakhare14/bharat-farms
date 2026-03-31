6de285bf7d04b5cc8fbd029ada9e100d4e013b33
surajsakhare14 <surajsakhare142002@gmail.com>
2026-03-31 22:05:01 +0530

    add hamburger, mobile responsive

--- static/js/script.js
+++ static/js/script.js
@@ -14,3 +14,18 @@ window.addEventListener("scroll", showCards);
 
 /* 🔥 RUN ON PAGE LOAD */
 window.addEventListener("load", showCards);
+
+/* HAMBURGER MENU */
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+/* CLOSE MENU ON CLICK */
+document.querySelectorAll("#nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
